cd9e7f3a195c295fdb8838ace8891d50af01d089
Hayelom Tizazu <hayelomtizazu19@gmail.com>
2021-11-03 10:11:29 +0300

    temp

--- public/script.js
+++
@@ -1,6 +0,0 @@
-const burger = document.querySelector('#burger');
-const menu = document.querySelector('#menu');
-
-burger.addEventListener('click', function () {
-  menu.classList.toggle('hidden');
-});
